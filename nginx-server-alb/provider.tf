2a05506afb1c802734e2e87db05de7bb31518c8f
root <root@ip-172-31-41-71.us-east-2.compute.internal>
2021-08-30 14:21:10 +0000

    Latest-Code2

--- nginx-server-alb/provider.tf
+++ nginx-server-alb/provider.tf
@@ -1,6 +1,6 @@
 # Define AWS as our provider
 provider "aws" {
   region = "${var.aws_region}"
-  access_key = "AKIAZL322Q7YWNN24APM"
-  secret_key = "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"
+  access_key = "Please Add IAM-User_Developer-access-Key"
+  secret_key = "Please Add IAM-User_Developer-secret-Key"
 }
--- nginx-server/provider.tf
+++ nginx-server/provider.tf
@@ -1,6 +1,6 @@
 # Define AWS as our provider
 provider "aws" {
   region = "${var.aws_region}"
-  access_key = "AKIAZL322Q7YWNN24APM"
-  secret_key = "5rFJ4D3ixWDDY1Mn9pb333ooR9hx42Y/KOHvI/Ma"
+  access_key = "Please add your IAM access Key"
+  secret_key = "Please add your IAM secret Key"
 }
